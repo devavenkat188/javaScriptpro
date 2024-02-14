183da04883db995c549915393213b4a66f35e75e
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 15:59:52 -0800

    If_else.js

---
+++ if_else.js
@@ -0,0 +1,8 @@
+let balanceAmount=3000;
+let message;
+if (balanceAmount>1000) {
+    message="Available Amount +`[balanceAmount]`";
+}
+else {
+    message="Minimum Balance Amount Reached. Please Add Amount";
+}
