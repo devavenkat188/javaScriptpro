4f522445c29bb6aa8a68e7cef8c93b46abc71fef
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 10:19:34 -0800

    variable.js

--- variable1.js
+++ variable1.js
@@ -1,1 +1,9 @@
+let fullName="Deva";
+let age=25;
+let aadharActivate="yes";
+let mobileNumber;
 
+console.log(fullName);
+console.log(age);
+console.log(aadharActivate);
+console.log(typeof(mobileNumber));
