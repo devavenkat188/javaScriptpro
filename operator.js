d4ef0b3b11a6b35386402d60185ef90d9acc8741
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 10:44:52 -0800

    operator.js

---
+++ operator.js
@@ -0,0 +1,15 @@
+let value=15;
+let value2=30;
+let addition=value+value2;
+let div=value/value2;
+let modulus=value%value2;
+let multi=value*value2;
+let sub=value2-value;
+let equal=value===value2;
+
+console.log(addition);
+console.log(div);
+console.log(modulus);
+console.log(multi);
+console.log(sub);
+console.log(equal);
