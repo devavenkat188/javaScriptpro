8dc197bbcd69454e2fb8d1db159431887745e603
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 12:28:16 -0800

    String.js

--- string.js
+++ string.js
@@ -1,4 +1,14 @@
 let myVariable="Computer Science";
 
-console.log(myVariable.length());
-// console.log(myVariable.charAt(8));
+console.log(myVariable.length);
+console.log(myVariable.charAt(10));
+console.log(myVariable.indexOf("e"));
+console.log(myVariable.lastIndexOf("e"));
+console.log(myVariable.slice(3,10)); // it does n't display the 10 th index position character.
+console.log(myVariable.slice(11));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("Com"));
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""));
+
