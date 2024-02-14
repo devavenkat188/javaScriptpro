e1c6cac5f29944a326087289be4488f841e4b325
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 10:22:20 -0800

    variable.js

--- variable1.js
+++ variable1.js
@@ -2,8 +2,10 @@ let fullName="Deva";
 let age=25;
 let aadharActivate="yes";
 let mobileNumber;
+let sum=age+mobileNumber;
 
 console.log(fullName);
 console.log(age);
 console.log(aadharActivate);
 console.log(typeof(mobileNumber));
+console.log(sum);
