21c752f831dc8da0ed0ea2ca44ef41f8974829be
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 14:47:35 -0800

    math.js

--- math.js
+++ math.js
@@ -2,8 +2,21 @@ const myValue=56.7;
 const piValue=Math.PI;
 
 console.log(Math.PI);
-console.log(Math.trunc(piValue));
-console.log(Math.round(myValue));
-console.log(Math.ceil(myValue));
-console.log(Math.floor(myValue));
-console.log(Math.pow(5,3));
+console.log(Math.trunc(piValue)); // It will display only Integer value.
+console.log(Math.round(myValue)); // It will display round off value based on 0.5 above and below.
+console.log(Math.ceil(myValue)); // It will display next upper value 
+console.log(Math.floor(myValue)); // display lower value
+console.log(Math.pow(5,3)); // exponentiation
+console.log(Math.random()); // assign random values
+console.log(Math.floor(Math.random()*10)+1); // Print 0 - 10 random values.
+
+/*
+3.141592653589793
+3
+57
+57
+56
+125
+0.10866472187004028
+10
+*/
