14347ff0cc67a5dafd3ac27209a88a6ead6f60cc
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 13:56:48 -0800

    math.js

---
+++ math.js
@@ -0,0 +1,9 @@
+const myValue=56.7;
+const piValue=Math.PI;
+
+console.log(Math.PI);
+console.log(Math.trunc(piValue));
+console.log(Math.round(myValue));
+console.log(Math.ceil(myValue));
+console.log(Math.floor(myValue));
+console.log(Math.pow(5,3));
