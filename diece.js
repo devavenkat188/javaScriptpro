b10f23a41bf53bb3b02d434b8861ee9161ecfeae
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 11:18:30 -0800

    diece.js

---
+++ diece.js
@@ -0,0 +1,6 @@
+let player = (Math.floor(Math.random()*6));
+let computer = (Math.floor(Math.random()*6));
+let result;
+if(player == 1 || computer == 1 && player > 1 || computer > 1){
+    result = "Your Game Begins"
+}
