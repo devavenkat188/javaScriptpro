c801e98c6058d376bd63a2b22fa79e195aa9d024
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 11:06:47 -0800

    ternary_game_pro.js

--- ternary_game_pro.js
+++ ternary_game_pro.js
@@ -2,5 +2,5 @@ let player = "rock";
 let computer = "pen";
 let result;
 
-result = player === computer ? "Tie" : player === "rock" && computer === "sicssors" ? "Player wins" : player === "rock" && computer === "paper" ? "Computer wins" : "Player wins";
+result = player === computer ? "Tie" : player === "rock" && computer === "scissors" ? "Player wins" : player === "rock" && computer === "paper" ? "Computer wins" : "Player wins";
 console.log(result);
