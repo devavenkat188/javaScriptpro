5c8605bbfe7f5c2e3d1878e0dd3368731f4968a6
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 10:01:51 -0800

    ternary_operator.js

--- ternary_operator.js
+++ ternary_operator.js
@@ -1,3 +1,4 @@
-const age = 25;
-const person = age >=18 ? "You are elgible to Vote" : "You are not elgible for vote";
-console.log(person);
+const age = (Math.floor(Math.random()*20));
+const person = age >=18 ? "You are elgible to Vote" : "You are not elgible for vote" ;
+
+console.log(person +" "+`${age}`);
