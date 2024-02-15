979562318d53da0ecaf81a38d7d6333aa61bda11
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 18:45:40 -0800

    var_let_const.js

--- var_let_const.js
+++ var_let_const.js
@@ -1,15 +1,26 @@
 if (true){
     var varVariable="This is true";
 }
+    var varVariable="The var variable can be re-declare";
 
 console.log(varVariable);
 
 if(true) {
-    let varVariable="This is used only Inside a block";
-    console.log(varVariable);
+    let letVariable="This is used only Inside a block";
+    
+    console.log(letVariable);
 }
 
+
+
 if(true){
-    const varVariable="This is constant";
+    const conVariable="This is also block scoped variables";
+    console.log(conVariable);
 }
-console.log(varVariable);
+
+const constVar = {name: "Deva"};
+let letVar = 1;
+
+constVar.name = "Shankar";
+
+console.log(constVar); // we can modify the properties of the class created by using the const variable
