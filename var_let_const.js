2c63c138290a9dcec9e61af46aa6683d55b2dfb6
devavenkat188 <devavenkat188@gmail.com>
2024-02-14 18:30:28 -0800

    var_let_const.js

---
+++ var_let_const.js
@@ -0,0 +1,15 @@
+if (true){
+    var varVariable="This is true";
+}
+
+console.log(varVariable);
+
+if(true) {
+    let varVariable="This is used only Inside a block";
+    console.log(varVariable);
+}
+
+if(true){
+    const varVariable="This is constant";
+}
+console.log(varVariable);
