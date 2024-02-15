9872af9e79b91fff760017a7d6bf78f335787aea
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 09:24:49 -0800

    switch1.js

---
+++ switch1.js
@@ -0,0 +1,10 @@
+switch(Math.floor(Math.random()*4)) {
+    case 1:
+        console.log("Welcome to ICICI bank");
+    case 2:
+        console.log("Welcome to SBI");
+    case 3:
+        console.log("Welcome to KVB");
+    default:
+            console.log("Your are different bank customer");
+}
