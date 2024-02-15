ab32f59d86cb7e267b5bd592f1b4373c7fd034e0
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 10:29:54 -0800

    ternary2.js

--- ternary2.js
+++ ternary2.js
@@ -1,5 +1,6 @@
 let player = (Math.floor(Math.random()*6));
 let computer = (Math.floor(Math.random()*6));
 let diece;
-diece = player ===1 ? "Game Starts" : computer === 1 ? "Automatically Game starts" : palyer >=1 ? player+player+" Round-2 Player" : computer >=1 ? computer+computer+" Round-2 Computer" : "Not Match";
+diece = player ===1 ? "Game Starts by Player" : computer === 1 ? "Automatically Game starts" : "Game Ends";
+// diece = player ===1 ? "Game Starts" : computer === 1 ? "Automatically Game starts" : player >=1 ? player+1+" Round-2 Player" : computer >=1 ? computer+1+" Round-2 Computer" : "Not Match"; 
 console.log(diece);
