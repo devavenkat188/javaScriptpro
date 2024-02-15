cc0a71b868be7b1458485e74f951358ea933931e
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 12:00:25 -0800

    user_input2.js

--- user_input2.js
+++ user_input2.js
@@ -1,2 +1,4 @@
 let fullName = prompt("Enter your full name");
-console.log(fullName);
+console.log(fullName); // It will return either our name or if we give cancel it will return null value
+console.log(fullName ?? "You didn't Enter your Name"); // Nullified operator --> it will return the message
+// instead of null as the output.
