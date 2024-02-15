191553f182cb7eed243c22cbf0320c9bd110dc2e
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 10:46:00 -0800

    test_score_ternary.js

--- test_score_ternary.js
+++ test_score_ternary.js
@@ -1,5 +1,5 @@
 let testScore = (Math.floor(Math.random()*100));
 let grade;
 
-grade = testScore >= 90 ? "A grade" : testScore >= 80 ? "B grade" : testScore >= 70 ? "C grade" : testScore >= 60 ? "D grade" : testScore >= 50 ? "RA" : testScore >=40 ? "F" : "Fail";
-console.log(grade);
+grade = testScore >= 90 ? "A grade" : testScore >= 80 ? "B grade" : testScore >= 70 ? "C grade" : testScore >= 60 ? "D grade" : testScore >= 40 ? "Re-Evaluation" : "Fail";
+console.log(grade +" "+ `${testScore}`);
