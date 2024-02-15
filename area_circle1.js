8ed99f884456cff42500585ac9162e0fbb0ec0fb
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 15:22:27 -0800

    area_circle1.js

--- area_circle1.js
+++ area_circle1.js
@@ -1,3 +1,4 @@
 var radius = 4;
+var radius = 6; // --> by using var the redeclare of the same variable is possible.
 var circle = Math.PI*radius*radius;
 console.log(`Area of the Circle = ${circle}`);
