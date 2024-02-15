d1c438979e2df47b230a6b650b72bcc0a692a1da
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 15:58:06 -0800

    explicit.js

--- explicit.js
+++ explicit.js
@@ -1,11 +1,15 @@
 let bigInt = 5n;
-let numvar = 3;
+let numvar = 9;
+let check;
 let total = bigInt + BigInt(numvar); // --> BigInt and normal Number can not be add dirctly. so used Explicit type conversion.
 let add = Number(bigInt) + numvar;
+check = bigInt >= numvar; // --> boolean will be work fine with bigInt.
 console.log(total);
 console.log(add);
+console.log(check);
 
 /*
 8n
 8
+false
  */
