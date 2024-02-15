94925aab14f4db58dd2b99378cd915d40d095112
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 15:52:41 -0800

    explicit.js

--- explicit.js
+++ explicit.js
@@ -1,4 +1,11 @@
 let bigInt = 5n;
 let numvar = 3;
-let total = bigInt + numvar;
+let total = bigInt + BigInt(numvar); // --> BigInt and normal Number can not be add dirctly. so used Explicit type conversion.
+let add = Number(bigInt) + numvar;
 console.log(total);
+console.log(add);
+
+/*
+8n
+8
+ */
