9324fc062e2b7ed49e9e23cb21aa6e3661d1a6a9
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 20:48:59 -0800

    ternary_working_hrs.js

---
+++ ternary_working_hrs.js
@@ -0,0 +1,3 @@
+let workingHours = 9.20;
+let additionalWorkingHours = workingHours > 9.20 ? "you have additional working hours": "you have negative working hours";
+console.log(additionalWorkingHours);
