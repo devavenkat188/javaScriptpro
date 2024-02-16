b410b2bd38331db57c954804b75dac5e0884eaaf
devavenkat188 <devavenkat188@gmail.com>
2024-02-16 10:54:08 -0800

    for_1.js

--- for_1.js
+++ for_1.js
@@ -1,10 +1,5 @@
 let name = "Devashankar";
 for(let i=0;i<=name.length;i++){
-    if(name === 'a' || name === 'e' || name ==='i' || name === 'o' || name === 'u'){
-        console.log(name.charAt(i));
-    }
-    else {
-        console.log(name.charAt(i));
-    }
     
+        console.log(name.charAt(i));
 }
