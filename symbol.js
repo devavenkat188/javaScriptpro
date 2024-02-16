d363397c0852038e27f0ed926fa9d16a04cf4bd3
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 16:21:45 -0800

    symbol.js

--- symbol.js
+++ symbol.js
@@ -1,10 +1,14 @@
-let empid = Symbol("Deva");
+let empid = Symbol("Deva"); // Symbol() --> represent a unique identifier.
 let empid2 = Symbol(12);
 let result = empid;
 console.log(result);
 console.log(typeof(result));
+console.log(empid2.toString()); // it wil be forefully we converted symbol into a string type.
+console.log(typeof(empid2.toString())); 
 
 /*
 Symbol(Deva)
 symbol
+Symbol(12)
+string
  */
