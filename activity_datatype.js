bbe1ba8bdb8516ec91fa2b0c4ea8fa3b259985be
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 16:49:30 -0800

    activity_datatype.js

---
+++ activity_datatype.js
@@ -0,0 +1,14 @@
+let firstName = "Devashankar";
+let lastName = "Venkatachalam";
+let age = 25;
+console.log(`Employee Details:
+            First Name: ${firstName}
+            Last Name: ${lastName}
+            Age: ${age}`); // template Literals.
+
+/*
+Employee Details: 
+            First Name: Devashankar
+            Last Name: Venkatachalam
+            Age: 25
+ */
