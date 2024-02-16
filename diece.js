dd6745041e77a1a831a31720dabd91fb2dd5e4c3
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 21:26:16 -0800

    diece.js

--- diece.js
+++ diece.js
@@ -1,10 +1,19 @@
 let player = (Math.floor(Math.random()*6));
-let computer = (Math.floor(Math.random()*6));
+let value = 100;
+let pos;
 let result;
-if(player == 1){
-    result = `Your Game Begins ${player}`;
+if(player === 1) {
+    result = "Enter into the Game";
+    if (player <= 6 && value != 0) {
+        pos = value - player;
+    }
+    else if (player > 6 && value != 0) {
+        pos = value - player;
+    }
+    else if (player > 6 && value != 0) {
+        pos = value - player;
+    }
+    else if (player > 6 && value != 0) {
+        pos = value - player;
+    }
 }
-else if (computer == 1) {
-    result = `Computer game begins ${computer}`;
-}
-console.log(result);
