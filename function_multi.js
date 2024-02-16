e4b8588c44789beb49824e27a0b950bd73e622f1
devavenkat188 <devavenkat188@gmail.com>
2024-02-16 15:18:24 -0800

    function_multi.js

--- function_multi.js
+++ function_multi.js
@@ -1,4 +1,12 @@
+/*
 function multi(a,b){
     return a*b;
 }
 console.log(multi(3,4));
+*/
+
+let mail = "devavenkat188@gmail.com"
+function gmail(mail){
+    return mail.slice(0,mail.indexOf("@"));
+}
+console.log(gmail(`${mail}`));
