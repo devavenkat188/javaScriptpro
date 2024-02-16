df419064a911b366a815975f7c35e588228c176e
devavenkat188 <devavenkat188@gmail.com>
2024-02-15 19:40:54 -0800

    interactive_game.js and interactive_game.html

---
+++ interactive_game.js
@@ -0,0 +1,51 @@
+let playaGame = confrim("Shall we played rock, paper or scissors ?");
+if(playaGame) {
+    let playerChoice = prompt("please enter rock, paper or scissors");
+    if(playerChoice) {
+        let player1 = playerChoice.trim().toLocaleUpperCase();
+        if(player1 === "rock" ||
+           player1 === "paper" ||
+           player1 === "scissors") {
+
+            let computerChoice = Math.floor(Math.random()*3);
+            let computer;
+            if(computerChoice === 1) {
+                computer = "rock";
+            }
+            else if (computerChoice === 2) {
+                computer = "paper";
+            }
+            else {
+                computer = "scissors";
+            }
+
+            let result;
+            if( player1 === computer) {
+                result = "Tie Game";
+            }
+            else if(player1 === "paper" && computer === "rock" ) {
+                result = "Player Wins";
+            }
+            else if(player1 === "paper" && computer === "scissors") {
+                result = "Player Wins";
+            }
+            else if(player1 === "rock" && computer === "scissors") {
+                result = "Player Wins";
+            }
+            else {
+                result = "Computer Wins";
+            }
+            alert(result);
+            let playAgain = confirm ("play Again?");
+            playAgain ? location.reload() : alert("Ok, Thanks for Playing.");
+
+           } else {
+                alert ("You didn't enter rock, Paper or scissors");
+           }
+    } else {
+        alert ("I guess you can change your mind. May be next time");
+    }
+}
+else {
+    alert ("Ok, Maybe next time");
+}
