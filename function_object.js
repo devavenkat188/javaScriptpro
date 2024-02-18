99c5d0fd3d8aaca4e97add367b70afe261822ae5
devavenkat188 <devavenkat188@gmail.com>
2024-02-17 21:16:33 -0800

    function_object.js

---
+++ function_object.js
@@ -0,0 +1,18 @@
+const person1 = {
+    name: "Deva",
+    age: 25,
+};
+
+const person2 = {
+    name: "Sanjay",
+    age: 22,
+};
+
+function printPerson(person) {
+    console.log(person.name);
+    console.log(person.age); 
+}
+
+printPerson(person1);
+printPerson(person2);
+printPerson({name: "Arun", age: 25});
