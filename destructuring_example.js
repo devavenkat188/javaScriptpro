c5281de292915454f0b4525dace6459a0f013749
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 13:48:58 -0800

    destructuring_example.js

---
+++ destructuring_example.js
@@ -0,0 +1,8 @@
+let names = ['Devashankar','Sanjay','Arun','Amul'];
+
+const [name1,name2, ,name4] = names; // name1 .. --> are the variables.
+// follow the order to skip the names in-between.
+
+console.log(name1);
+console.log(name2);
+console.log(name4);
