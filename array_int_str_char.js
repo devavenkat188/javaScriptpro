3da9502fd20c359a426be0b5874bb161223721b1
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 11:58:57 -0800

    array_int_str_char.js

---
+++ array_int_str_char.js
@@ -0,0 +1,6 @@
+let arr = [4,5,6,7,'a','b',"Deva",[3,4,5]]; // combination of different datatypes.
+
+console.log(arr);
+console.log(arr.length);
+console.log(arr[arr.length-1][0]);
+console.log(arr[arr.length-2]);
