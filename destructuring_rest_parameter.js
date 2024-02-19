25144fa5029cb756656b748c8e4b8320c52cf12d
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 15:03:10 -0800

    destructuring_rest_parameter.js

--- destructuring_rest_parameter.js
+++ destructuring_rest_parameter.js
@@ -2,7 +2,7 @@ const names = [undefined,'Deva','Sanjay','Kishore'];
 
 const [name1 = "Default Name", ...name2] = names; // ...name2 = rest parameter --> it will defined within the array.
 const [name = "Default Name", ...[nam1]] = names; // ...[nam1] --> it will select the array of first element.
-const [nam = "Default Name", ...[,,val3]] = names; // ...[,,val2]] --> it will print                                                                                                                                                                                                                               
+const [nam = "Default Name", ...[,,val3]] = names; // ...[,,val2]] --> it will print the last name.                                                                                                                                                                                                                             
 console.log(name1);
 console.log(name2);
 console.log(nam1);
