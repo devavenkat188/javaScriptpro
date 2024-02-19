f1db7c795e80425570df275c67cb4a5f9e0537de
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 15:21:14 -0800

    obj_destructuring.js

---
+++ obj_destructuring.js
@@ -0,0 +1,10 @@
+const employee = {
+    firstName: 'Devashankar',
+    lastName: 'V',
+    age: 25,
+    isMajor: true,
+};
+
+const{firstName} = employee;
+console.log(employee.firstName);
+console.log(firstName); // because the objedt  is created in the above line.
