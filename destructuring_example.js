a2cf70fce931f5bba10e659d7be81cec74f7d111
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 14:32:04 -0800

    destructuring_example.js

--- destructuring_example.js
+++ destructuring_example.js
@@ -1,6 +1,7 @@
-let names = ['Devashankar','Sanjay','Arun','Amul'];
+let names = ['Devashankar','Sanjay','Arun',undefined]; // there is undefined means we can set own name to the
+// variables.
 
-const [name1,name2, ,name4] = names; // name1 .. --> are the variables.
+const [name1,name2, ,name4 = "Default Name"] = names; // name1 .. --> are the variables.
 // follow the order to skip the names in-between.
 
 console.log(name1);
