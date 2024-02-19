fc9e1f3cbe9a521b1d9088caa7ae3daee7922e47
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 10:39:32 -0800

    inner_function.js

---
+++ inner_function.js
@@ -0,0 +1,11 @@
+function addition (value1,value2){
+    function divison (value3,value4) { // Inner function --> will be defined only in the inside of the function.
+        const number = value3 / value4;
+        return number;
+    }
+    console.log(divison(10,2));
+
+    const num = value1 + value2;
+    return num;
+}
+console.log(addition(6,7));
