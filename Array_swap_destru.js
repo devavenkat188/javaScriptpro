f6f5e3502ffee501fa8148632e2a4a75bf1653ac
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 15:54:33 -0800

    Array_swap_destru.js

--- Array_swap_destru.js
+++ Array_swap_destru.js
@@ -6,7 +6,7 @@ let arr = [a,b];
 [b,a] = arr;
 */
 
-[b,a] = [a,b];
+[b,a] = [a,b]; // in array [b,a] --> we can use any variable.but in object it is not possible.
 
 // console.log(arr);
 console.log(a,b);
