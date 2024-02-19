d7d4c68d7195ed47047ad23665b48261244c6024
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 11:04:30 -0800

    arrow_function.js

---
+++ arrow_function.js
@@ -0,0 +1,9 @@
+/* 
+const getFullName = (firstName,lastName) => { // if has multiple lines use this method.
+    return (firstName+" "+lastName);
+}
+*/
+const getFullName = (firstName,lastName) => firstName+" "+lastName; // if the function has only one line
+// the statement will be written like this.
+let fullName = getFullName("Devashankar","V");
+console.log(fullName);
