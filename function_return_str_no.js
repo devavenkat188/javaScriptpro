e52bcb80b3405707af5c1a7e6ef900644d0be0f5
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 10:12:43 -0800

    function_return_str_no.js

---
+++ function_return_str_no.js
@@ -0,0 +1,11 @@
+function getFullName(firstName,lastName) {
+    const name = firstName+" "+lastName;
+    return name;
+}
+function getVaule(number1,number2) {
+    const number = number1 + number2;
+    return number;
+}
+let total = getVaule(4,6);
+console.log(getFullName("Deva","V")); // to return a String
+console.log(total); // to return a Values.
