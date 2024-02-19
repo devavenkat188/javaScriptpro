0fe7e8b0e8c2ee6b09b41f8b5dd87582742c37a4
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 11:49:23 -0800

    array_ex.js

---
+++ array_ex.js
@@ -0,0 +1,7 @@
+let cities = ["Chennai","Coiambatore","Salem","Erode"];
+let marks = [60,90,80,78,89,65];
+
+console.log(cities.length); // array.length() --> used to find length of the array.
+console.log(cities[1]); // print the value based on their index position.
+console.log(marks);
+console.log(marks[marks.length-1]); // print the last element of the array, if the leght is unknown.
