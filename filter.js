8a9dbbfceffaeab9b8f5c4ab9229ea16f4e33c14
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 18:03:06 -0800

    filter.js

--- filter.js
+++ filter.js
@@ -1,6 +1,7 @@
 let itemCost = [35,55,270,98,100,190,460,8,20];
 
-let minimalCostLessthan100 = itemCost.filter(x => x <= 100);
+let minimalCostLessthan100 = itemCost.filter(x => x <= 100); // filter --> will work based on the condition
+// we give.
 
 console.log(minimalCostLessthan100);
 console.log(minimalCostLessthan100.length);
