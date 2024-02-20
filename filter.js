8fb114e4295f779e30c5d4e887385a5ce05500da
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 18:01:55 -0800

    filter.js

---
+++ filter.js
@@ -0,0 +1,6 @@
+let itemCost = [35,55,270,98,100,190,460,8,20];
+
+let minimalCostLessthan100 = itemCost.filter(x => x <= 100);
+
+console.log(minimalCostLessthan100);
+console.log(minimalCostLessthan100.length);
