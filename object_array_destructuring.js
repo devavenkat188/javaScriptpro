a679186caa561d4b8137e88657097c3bcb783a0c
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 17:08:33 -0800

    object_array_destructuring.js

---
+++ object_array_destructuring.js
@@ -0,0 +1,11 @@
+const employee = {
+    company1: ['Deva','Sanjay'],
+    company2: ['Arun','Amul']
+}
+
+const {company1:[name1 , name2], company2: [name3, name4]} = employee;
+
+console.log(name1);
+console.log(name2);
+console.log(name4);
+console.log(name3);
