89f624a735668b3fc659d493c30063ed0fb83b2e
devavenkat188 <devavenkat188@gmail.com>
2024-02-20 12:30:08 -0800

    class_dynamic_fun.js

--- class_dynamic_fun.js
+++ class_dynamic_fun.js
@@ -1,7 +1,18 @@
 class Employee {
-    
+    // employeeName; // in javascript global variable is taken by default. so we don't need to define.
     print(name) { // dynamic function -- by adding name as a parameter.
+        this.employeeName = name;
         console.log(name);
+    }
+    
+    specialPrint() {
+        console.log("Special Print = "+this.employeeName);
+    }
+    addition() {
+        this.value1 = 10;
+        this.value2 = 20;
+        this.value3 = this.value1+this.value2;
+        console.log(this.value3);
     }
 }
 let employee = new Employee();
@@ -9,3 +20,6 @@ employee.print("Deva");
 
 let emp2 = new Employee();
 emp2.print("Sanjay");
+
+employee.specialPrint();
+employee.addition();
