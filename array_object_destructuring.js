fca9de352bc8d0916f80f80c2910f15d35cbb37e
devavenkat188 <devavenkat188@gmail.com>
2024-02-19 17:01:12 -0800

    array_object_destructuring.js

---
+++ array_object_destructuring.js
@@ -0,0 +1,11 @@
+const employee = [
+    {name : 'Devashankar',age: 25},
+    {name: 'Sanajy',age: 22},
+    {name: 'Arun',age: 25}
+];
+
+[{age: name1Age},{age: name2Age},{age: name3Age}] = employee;
+
+console.log(name1Age);
+console.log(name2Age);
+console.log(name3Age);
