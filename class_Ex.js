4286b1de0240c586a0d4d928f9f783397cae9a58
devavenkat188 <devavenkat188@gmail.com>
2024-02-20 12:01:02 -0800

    class_Ex.js

--- class_Ex.js
+++ class_Ex.js
@@ -1,3 +1,17 @@
 class Employee {
-    
+    employeeName = 'Devashankar';
+
+    print() {
+        console.log(this.employeeName);
+    }
 }
+
+let employee = new Employee(); // employee & employee2 is instance variable by accessing the instance 
+// variable using this keyword.
+employee.print();
+// console.log(employee.employeeName);
+
+let employee2 = new Employee();
+employee2.employeeName = "Sanjay";
+employee2.print();
+// console.log(employee2.employeeName);
