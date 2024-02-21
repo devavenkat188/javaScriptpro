6681b2d26fa3297346b0ce719147d2621e8594e6
devavenkat188 <devavenkat188@gmail.com>
2024-02-20 18:53:26 -0800

    object_deep_copy_cloning.js

---
+++ object_deep_copy_cloning.js
@@ -0,0 +1,11 @@
+let age = 25;
+let newAge = age;
+age ++;
+console.log("Age : "+age); // new incremented value
+console.log("newAge : "+newAge); // remains the same initial value.
+
+/* In primitve datatype the deep copy object is achieved by assign the one variable directly to the another variable
+at initial what the value given to the 1st variable is remain constant in the assigning of another variable.
+here the first variable value is changed & second variable value remains unchanged & holds the same value at
+initial first variable value.
+*/
