51ad89cb55294aa03c4e6bc19f8d67b0fd5d1160
devavenkat188 <devavenkat188@gmail.com>
2024-02-20 19:05:03 -0800

    shallow_copy_object

---
+++ shallow_copy_object_cloning.js
@@ -0,0 +1,9 @@
+let age = {value: 25};
+let newAge = age;
+
+age.value++; // value changes at initial object.
+
+console.log("Age ");
+console.log(age);
+console.log("newAge ");
+console.log(newAge); // shalow copy object cloning.
