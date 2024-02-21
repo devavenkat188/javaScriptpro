9069bbec5a93cb14fbd7e413c80590204fc2745b
devavenkat188 <devavenkat188@gmail.com>
2024-02-20 21:50:02 -0800

    deep_copy_object_spread.js

--- deep_copy_object_2.js
+++
@@ -1,9 +0,0 @@
-let age = {value : 25};
-let newAge = JSON.parse(JSON.stringify(age)); // JSON.stringify(age) --> will convert object into string
-// JSON.parse(age) --> will convert string into the object.
-age.value++;
-
-console.log("Age ");
-console.log(age);
-console.log("newAge ");
-console.log(newAge);
