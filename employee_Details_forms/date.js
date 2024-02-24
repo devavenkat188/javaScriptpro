182c33b9730b965bc7e4d95b3c777e6e194f9416
devavenkat188 <devavenkat188@gmail.com>
2024-02-24 13:03:10 -0800

    date.js

--- employee_Details_forms/date.js
+++ employee_Details_forms/date.js
@@ -1,2 +1,14 @@
 let d = new Date();
-console.log(d);
+console.log(typeof(d));
+
+let [month, date, year] = new Date().toLocaleDateString("en-US").split("/");
+console.log(month, date, year);
+
+var today = new Date();
+var validMinDate = new Date(
+    today.getFullYear()-18,
+    today.getMonth(),
+    today.getDate(),
+    today.getHours(),
+    today.getMinutes(),
+);
--- employee_Details_forms/form_validation.js
+++ employee_Details_forms/form_validation.js
@@ -8,6 +8,8 @@ const position = document.getElementById("position");
 const address = document.getElementById("address");
 const experience = document.getElementById("experience");
 const cv = document.getElementById("cv");
+const box = document.getElementById("box");
+const select = document.getElementById("dept");
 /*
 form.addEventListener('submit',(e)=>{
     if(!validateInputs()) {
@@ -32,20 +34,28 @@ function validateInputs() {
     const experienceVal = experience.value.trim();
     const fileVal = cv.value.trim();
     const dateVal = date.value.trim();
+    let success = true;
+   // const boxVal = box.value.trim();
+   // const selectVal = select.value.trim();
 
     if(firstNameVal === ''){
+        success = false;
         setError(firstName, "FirstName is Required");
     } else if (firstNameVal.length < 4) {
+        success = false;
         setError(firstName, "FirstName should be minimum 4 characters");
     } else if(firstNameVal.length > 14) {
+        success = false;
         setError(firstName, "FirstName should not exceeds to 14 characters");
     } else {
         setSuccess(firstName);
     }
 
     if(lastNameVal === ''){
+        success = false;
         setError(lastName, "LastName is Required");
     } else if(!validateLastName(lastNameVal)) {
+        success = false;
         setError(lastName, "LastName will be in uppercase & 1 Character Only");
     }
     else {
@@ -53,8 +63,10 @@ function validateInputs() {
     }
 
     if(emailVal === ''){
+        success = false;
         setError(email, "Email is Required");
     } else if(!validateEmail(emailVal)) {
+        success = false;
         setError(email, "Please Enter a valid E-mail id");
     }
     else {
@@ -62,59 +74,88 @@ function validateInputs() {
     }
 
     if(phoneNumberVal === ''){
+        success = false;
         setError(phoneNumber, "Phone Number is Required");
     } else if (!validatePhoneNumber(phoneNumberVal)) {
+        success = false;
         setError(phoneNumber, "Phone Number must have 10 numbers");
     } else {
         setSuccess(phoneNumber);
     }
 
     if(positionVal === ''){
+        success = false;
         setError(position, "Position is required");
     }
     else if(positionVal.length > 15) {
+        success = false;
         setError(position, "Position should not exceeds 15 charatcers");
     } else if(positionVal.length <= 1) {
+        success = false;
         setError(position, "Position will be more than 1 character");
     } else {
         setSuccess(position);
     }
 
     if(addressVal === '') {
+        success = false;
         setError(address, "Address is Required.");
-    } else if(addressVal.length > 20) {
-        setError(address, "Address should not exceed 20 character");
+    } else if(addressVal.length > 50) {
+        success = false;
+        setError(address, "Address should not exceed 50 character");
     } else if(addressVal.length <= 5) {
+        success = false;
         setError(address, "Address will be more than 5 characters.")
     } else {
         setSuccess(address);
     }
 
     if(experienceVal === '') {
+        success = false;
         setError(experience, "Experience is Required.");
     } else if(!validateExperience(experienceVal)) {
+        success = false;
         setError(experience, "Experience range is between 1 to 15");
     } else {
         setSuccess(experience);
     }
 
     if(fileVal === ''){
+        success = false;
         setError(cv, "Please select the file");
     } else if(!validateCV(fileVal)) {
+        success = false;
         setError(cv, "Unsupported Format");
     }else {
         setSuccess(cv);
     }
 
     if(dateVal === ''){
+        success = false;
         setError(date, "Please select the Date");
     } else if(!validateDate(dateVal)) {
+        success = false;
         setError(date, "Date of birth can not be future");
     } else {
         setSuccess(date);
     }
     
-}
+    if(!validateCheckBox(box)) {
+        success = false;
+        setError(box, "Please select the CheckBox");
+    }
+    else {
+        setSuccess(box);
+    }
+
+    if(!validateDept(select)) {
+        success = false;
+        setError(select, "Please select the Department");
+    } else {
+        setSuccess(select);
+    } 
+    return success;
+} 
 function setError(element,message) {
     const inputGroup = element.parentElement;
     const errorElement = inputGroup.querySelector('.error');
@@ -124,7 +165,7 @@ function setError(element,message) {
 }
 function setSuccess(element) {
     const inputGroup = element.parentElement;
-    const errorElement = inputGroup.getElementById('.error');
+    const errorElement = inputGroup.querySelector('.error');
     errorElement.innerText = '';
     inputGroup.classList.add('success');
     inputGroup.classList.remove('error');
@@ -154,17 +195,30 @@ const validateExperience = (experience) => {
 };
 
 const validateCV = (cv) => {
-    return String(cv).match(/^(\.pdf|\.doc|\.docx)$/i) !== null;
+    return String(cv).match(/\.(pdf|doc|docx)$/i) !== null;
 };
 
 const validateDate = (date) => {
     const currentDate = new Date();
     const enteredDate = new Date(date);
-    if(enteredDate > currentDate) {
-        return validateDate();
+    if(enteredDate >= currentDate) {
+        return false;
 
     }
+    else {
+        return true;
+    }
 };
+
+ const validateCheckBox = (box) => {
+    return box.checked;
+};
+
+const validateDept = (select) => {
+    return select.options[select.selectedIndex].value !== "";
+};
+
+
 
 
 
