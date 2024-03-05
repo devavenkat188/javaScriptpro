e8432083bf6cc4572702023e3807f3081757e93f
devavenkat188 <devavenkat188@gmail.com>
2024-03-05 15:37:32 -0800

    anonymous_function.js

---
+++ Function/anonymous_function.js
@@ -0,0 +1,8 @@
+
+let getFullName = function(firstName,lastName) { // the entire function will be stored in a single variable of getFull
+    // Name --> Anonymous function.
+    let name = firstName+' '+lastName;
+    return name;
+}
+let fullName = getFullName('Deva','V');
+console.log(fullName);
