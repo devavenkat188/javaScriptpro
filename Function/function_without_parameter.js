816e226565c4658b2ec91c478c133b47cb83695d
devavenkat188 <devavenkat188@gmail.com>
2024-03-05 12:21:52 -0800

    function_without_parameter.js

---
+++ Function/function_without_parameter.js
@@ -0,0 +1,8 @@
+const person = {
+    name: 'Deva',
+    age: 25,
+};
+function printName() { // to define a function with function -- function name.
+    console.log(person.name);
+}
+printName(); // to call a function.
