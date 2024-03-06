c28eb6832341370b585015e847929218fdad2e94
devavenkat188 <devavenkat188@gmail.com>
2024-03-06 15:34:45 -0800

    server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,8 @@
-var http = require('http'); 
-
-http.createServer(function(request,response) {
-    response.write("Hello World!"); // to send back to the client.
-    response.end();
-}).listen(8080);
+var http = require('http'); // to access the http module.
+// create server is a function available in a module called http.
+http.createServer(function(request,response) { // createServer --> function, that will create a server for us.
+    response.writeHead(200,{'Content-Type':'text/html'}) // when we get response then you need to send the status 200, 
+// to get error - send the status 400, 404-file not found.
+    response.write("Hello World!"); // to send the response back to the client.
+    response.end(); // to indicate that our statement will be end. don't wait for excess statement
+}).listen(8080); // we need to mention the port number.
