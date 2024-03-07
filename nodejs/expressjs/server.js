1c26df31663ecf387cc3ee758ad8f2b80b70a53f
devavenkat188 <devavenkat188@gmail.com>
2024-03-06 18:41:09 -0800

    server.js

--- nodejs/expressjs/server.js
+++ nodejs/expressjs/server.js
@@ -3,7 +3,10 @@ const express = require('express'); // to get express
 const app = express(); // to start the express
 app.get('/',function(request,response){ // get --> router & '/' --> URL indication.
     response.send('Hello World'); // response --> data from server to client. & request --> data from client to server.
-})
+});
+app.get('/people',function(request,response){ // to activate different response by using router.
+    response.send('Welcome for Express Js learning');
+});
 // four methods - get, post, put & delete.
 app.listen(9000, function(request,response){  // to start the server.
         console.log('Running...') // to check wether the server is running / not.
