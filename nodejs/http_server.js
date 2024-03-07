a5432fd3c0589593084666d22602aa214de71d78
devavenkat188 <devavenkat188@gmail.com>
2024-03-06 16:13:22 -0800

    http_server.js

--- nodejs/http_server.js
+++ nodejs/http_server.js
@@ -1,13 +1,12 @@
-const http = require('node:http');
-const hostName = '127.0.0.1';
-const port = 3000;
+const http = require('node:http'); // access http from module.
+let hostname = '127.0.0.1'; // server name
+let port = 3000; // port number
 
-const server = http.createServer(function(request,response){
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    response.write('Welcome to NodeJS\n');
-    response.end();
-});
-server.listen(function(port,hostName){
-    console.log(`Server running at http://${hostName}:${port}/`);
+const server = http.createServer(function(request,response){ // create the server
+    response.statusCode = 200; // status code - to get the response
+    response.setHeader('Content-Type', 'text/plain'); // to metion the header with the content-type
+    response.write('Welcome to NodeJS\n'); // to send the response back to the client.
+    response.end(); // to indicate our response will end.
+});server.listen(port,hostname,function() { // mention the port number & servername with one empty function.
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
