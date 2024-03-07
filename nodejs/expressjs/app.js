c015f5ad9208e60ac01bf83ff76fbc7ad9ac6ad4
devavenkat188 <devavenkat188@gmail.com>
2024-03-07 14:43:54 -0800

    app.js

--- nodejs/expressjs/app.js
+++ nodejs/expressjs/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require('express'); 
 const app = express();
 const port = 8080;
 app.set('view engine','pug');
 app.set('views','./pugjs');
 app.get('/',(request,response)=>{
-    response.render('index');
+    response.render('index'); // to access the particular file.
 });
 app.listen(port,()=> {
     console.log(`Pug Server is running at http://localhost:${port}`);
