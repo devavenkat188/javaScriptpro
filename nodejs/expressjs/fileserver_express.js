2b2427ea25fc63ec1284b96328241a88f891480c
devavenkat188 <devavenkat188@gmail.com>
2024-03-07 12:11:12 -0800

    fileserver_express.js

--- nodejs/expressjs/fileserver_express.js
+++ nodejs/expressjs/fileserver_express.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require('express'); // to access the express
 const path = require('path');
-const app = express();
+const app = express(); // to start the express
 const port = 3000;
 
-app.get('/climate', function(request,response) {
-    response.sendFile(path.join(__dirname,'fileserver_expressjs','public', 'climate.html'));
-});
-app.get('/games',(request,response) => {
+app.get('/climate', function(request,response) { // to access the required file.
+    response.sendFile(path.join(__dirname,'fileserver_expressjs','public', 'climate.html')); // specify the path,
+}); // note --> HTML, CSS file will be available under the public folder.
+app.get('/games',(request,response) => { // to acccess the router to change the file.
     response.sendFile(path.join(__dirname, 'fileserver_expressjs','public','games.html'));
 });
 app.listen(port,function(){
