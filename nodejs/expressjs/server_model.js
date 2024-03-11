d847a80d467457de37de3d01693ab32b0bd029d5
devavenkat188 <devavenkat188@gmail.com>
2024-03-11 10:56:10 -0800

    server_model.js

--- nodejs/expressjs/server_model.js
+++ nodejs/expressjs/server_model.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const UserService = require('./sequelize/models/sequelize/model');
 
 module.exports = function(config) {
-    const UserService = new UserService(config.postgress.client);
+    const userService = new UserService(config.postgress.client);
 
 router.get('/',async(requset,response) => {
     try{
-        const user = await UserService.getUser();
+        const user = await userService.getUser();
         response.send(user);
     }
     catch(error){
