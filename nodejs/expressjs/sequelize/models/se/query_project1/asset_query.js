61815e443f08dc3a055d00a25f641cb2fdf3fbdc
devavenkat188 <devavenkat188@gmail.com>
2024-03-13 19:47:03 -0800

    tri_query.js

--- nodejs/expressjs/sequelize/models/se/query_project1/asset_query.js
+++ nodejs/expressjs/sequelize/models/se/query_project1/asset_query.js
@@ -1,11 +1,16 @@
 const {Sequelize, Model, DataTypes} = require('sequelize');
 const {sequelize} = require('../../../../config/db_connect');
+const Trip = require('./trip_query');
 
 const Asset = sequelize.define("asset", {
     Asset_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         allowNull: false,
+        references: {
+            model: 'trip',
+            key: 'Trip_id',
+        },
     },
     Owner_Name: {
         type: DataTypes.TEXT,
@@ -25,8 +30,11 @@ const Asset = sequelize.define("asset", {
 
 Asset.associate = function(models){
     Asset.belongsTo(models.Account);
+    Asset.hasOne(models.Trip);
 }
-
+Asset.hasOne(Trip, {
+    foriegnKey: 'Asset_id',
+});
 sequelize.sync({force: true});
 console.log('Table Created');
 module.exports = Asset;
---
+++ nodejs/expressjs/sequelize/models/se/query_project1/trip_query.js
@@ -0,0 +1,39 @@
+const {Sequelize, Model, DataTypes} = require('sequelize');
+const {sequelize} = require('../../../../config/db_connect');
+
+const Trip = sequelize.define("trip", {
+    Trip_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+    },
+    Trip_startl: {
+        type: DataTypes.TEXT,
+    },
+    Trip_endl: {
+        type: DataTypes.TEXT,
+    },
+    Loadin_time: {
+        type: DataTypes.TIME,
+    },
+    Loadout_time: {
+        type: DataTypes.TIME,
+    },
+    Unloadin_time: {
+        type: DataTypes.TIME,
+    },
+    Unloadout_time: {
+        type: DataTypes.TIME,
+    }
+    },{
+        tableName: "Trip",
+        timestamps: false,
+
+});
+
+Trip.associate = function(models){
+    Trip.belongsTo(models.Asset);
+}
+sequelize.sync({force: true});
+console.log('Table Created');
+module.exports = Asset;
