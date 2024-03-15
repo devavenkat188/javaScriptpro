f57407df46732af3f9562fa620ec1c824a952c63
devavenkat188 <devavenkat188@gmail.com>
2024-03-15 13:20:00 -0800

    asset_management_server.js

--- nodejs/expressjs/asset_management_server.js
+++ nodejs/expressjs/asset_management_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const User = require();
+const User = require('./sequelize/models/se/Asset_Management_Project/employee_master');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
