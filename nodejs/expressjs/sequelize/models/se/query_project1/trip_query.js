1355369e3f36f7d2337f377455ba90678c3aaa3c
devavenkat188 <devavenkat188@gmail.com>
2024-03-14 16:19:57 -0800

    trip_query.js

--- nodejs/expressjs/sequelize/models/se/query_project1/trip_query.js
+++ nodejs/expressjs/sequelize/models/se/query_project1/trip_query.js
@@ -31,7 +31,15 @@ const Trip = sequelize.define("Trip", {
     timestamps: false,
 
 });
+let values = [{ Trip_id: 1, Trip_startl: "Chennai", Trip_endl: "Pune", Loadin_time: '14:34', Loadout_time: '6:30', Unloadin_time: '10:50', Unloadout_time: '21:9', Asset_id: 2 },
+{ Trip_id: 2, Trip_startl: "Erode", Trip_endl: "Maharastra", Loadin_time: '7:32', Loadout_time: '11:50', Unloadin_time: '18:6', Unloadout_time: '22:08', Asset_id: 1 },
+{Trip_id: 3, Trip_startl: "Kashmir", Trip_endl: "Kerala", Loadin_time: '12:10', Loadout_time: '17:06', Unloadin_time: '8:04', Unloadout_time: '11:45', Asset_id: 3}];
 
+for(const value of values) {
+    if(value) {
+        Trip.create(value)
+    }
+}
 Trip.associate = function (models) {
     Trip.belongsTo(Trip, {
         foreignKey: 'Asset_id',
